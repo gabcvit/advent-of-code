a5ed819d626d453dab35a35d2c1ce0bfa6b460c9
Philipp Ludewig <diesdas@philippludewig>
2021-12-07 11:52:22 +0100

    [@gabcvit, @diesdasjenes] Solved Part 1

--- 2021/d_04/index.js
+++ 2021/d_04/index.js
@@ -51,25 +51,108 @@ const generateIndexTable = (tables) => {
 		}
 	})
 
-	// markIndex() {
-	// 	[63,[64],65].forEach(|e| => (tables[e.block][e.x][e.y])
-	// }
 	return indexTable
+}
+
+const markAllPositionOf = (number, bagOfNumbers, tables) => {
+
+	bagOfNumbers[number].forEach(indexedNumber => {
+		tables[indexedNumber.block][indexedNumber.x][indexedNumber.y].isMarked = true
+	})
+
+	return tables
+}
+
+const checkColumnOfTableForBingo = (indexOfColumn, table) => {
+	let isBingo = true;
+	for (let i = 0; i < 5; i++) {
+		isBingo = isBingo && table[i][indexOfColumn].isMarked;
+	}
+
+	return isBingo
+}
+
+const checkLineOfTableForBingo = (indexOfLine, table) => {
+	let isBingo = true;
+	for (let i = 0; i < 5; i++) {
+		isBingo = isBingo && table[indexOfLine][i].isMarked;
+	}
+	return isBingo
+}
+
+const findBingoBlock = (tables) => {
+	let indexOfBingoBlock = null
+
+	tables.forEach( (table, index) => {
+		for (let x = 0; x < 5; x++) {
+			if(table[x][0].isMarked === true) {
+				if (checkLineOfTableForBingo(x, table)) {
+					indexOfBingoBlock = index;
+					break;
+				 }
+			}
+		}
+		for (let y = 0; y < 5; y++) {
+			if(table[0][y].isMarked === true) {
+				if (checkColumnOfTableForBingo(y,table)) {
+					indexOfBingoBlock = index;
+					break;
+				}
+			}
+		}
+	})
+	// find block that has a full line or a full column with isMarked == true
+	return indexOfBingoBlock;
+}
+
+const solvePartOne = (filename) => {
+	const inputObject = generateInputFromFile(filename)
+	const bagOfNumbers = generateIndexTable(inputObject.tables)
+	let indexOfBingoBlock = null;
+	let lastNumber = null;
+	for (let index = 0; index < inputObject.drawNumbers.length; index++) {
+		inputObject.tables = markAllPositionOf(inputObject.drawNumbers[index],bagOfNumbers, inputObject.tables);
+		indexOfBingoBlock = findBingoBlock(inputObject.tables);
+		lastNumber = inputObject.drawNumbers[index];
+		if ( indexOfBingoBlock !== null) {
+			break;
+		}
+	}
+
+	let sum = 0;
+	inputObject.tables[indexOfBingoBlock].forEach(row => {
+		row.forEach(number => {
+			if(!number.isMarked) {
+				sum += number.value;
+			}
+		})
+	})
+	// console.log("Bingonumber: " + lastNumber + "BingoBlock: " + indexOfBingoBlock)
+	return sum * lastNumber
+}
+
+const solvePartTwo = (filename) => {
+
+	return 0
 }
 
 
 // RUN THE CHALLENGE
 
 // pt1
-// console.log('part 1: ', runPart1('input_final.txt'))
+console.log('part 1: ', solvePartOne('input_final.txt'))
 
 //pt 2
-// console.log('part 2: ', runPart2('input_final.txt'))
+// console.log('part 2: ', solvePartTwo('input_final.txt'))
 
 
 
 
 module.exports = {
 	generateInputFromFile,
-	generateIndexTable
+	generateIndexTable,
+	markAllPositionOf,
+	findBingoBlock,
+	solvePartOne,
+	solvePartTwo
 }
--- 2021/d_04/index.test.js
+++ 2021/d_04/index.test.js
@@ -1,5 +1,5 @@
 const { it, expect } = require("@jest/globals")
-const { generateInputFromFile,generateIndexTable } = require("./index.js")
+const { generateInputFromFile,generateIndexTable,markAllPositionOf, findBingoBlock, solvePartOne, solvePartTwo } = require("./index.js")
 
 
 describe('Testing index functionaities', () => {
@@ -25,8 +25,71 @@ describe('Testing index functionaities', () => {
 		expect(bagOfNumbers[7][0].block).toBe(0)
 	})
 
+	it('part1: should mark all numbers as true on their respective position', () => {
+		const inputObject = generateInputFromFile('input_example.txt')
+		const bagOfNumbers = generateIndexTable(inputObject.tables)
+
+		const updatedTables = markAllPositionOf(7,bagOfNumbers, inputObject.tables)
+
+		expect(updatedTables[0][2][4].isMarked).toBe(true)
+		expect(updatedTables[1][2][2].isMarked).toBe(true)
+		expect(updatedTables[2][4][4].isMarked).toBe(true)
+	})
+
+	it('part1: should find the bingo block in the first line', () => {
+		const inputObject = generateInputFromFile('input_example.txt')
+		const bagOfNumbers = generateIndexTable(inputObject.tables)
+
+		for (let i = 0; i <= 11; i++) {
+			inputObject.tables = markAllPositionOf(inputObject.drawNumbers[i], bagOfNumbers, inputObject.tables)
+		}
+
+		const blockIndex = findBingoBlock(inputObject.tables)
+
+		expect(blockIndex).toBe(2)
+	})
+
+	it('part1: should find the bingo block if the bingo is in the middle column', () => {
+		const inputObject = generateInputFromFile('input_example.txt')
+		const bagOfNumbers = generateIndexTable(inputObject.tables)
+
+		inputObject.drawNumbers = [0,15,13,7,10,9,16]
+
+		for (let i = 0; i <= 6; i++) {
+			inputObject.tables = markAllPositionOf(inputObject.drawNumbers[i], bagOfNumbers, inputObject.tables)
+		}
+
+		const blockIndex = findBingoBlock(inputObject.tables)
+
+		expect(blockIndex).toBe(1)
+	})
+
+	it('part1: should find the bingo block if the bingo is the first column', () => {
+		const inputObject = generateInputFromFile('input_example.txt')
+		const bagOfNumbers = generateIndexTable(inputObject.tables)
+
+		inputObject.drawNumbers = [0,22,8,21,10,1,7,6]
+
+		for (let i = 0; i <= 7; i++) {
+			inputObject.tables = markAllPositionOf(inputObject.drawNumbers[i], bagOfNumbers, inputObject.tables)
+		}
+
+		const blockIndex = findBingoBlock(inputObject.tables)
+
+		expect(blockIndex).toBe(0)
+	})
 
 
+	it('part1: should solve part one for test example', () => {
+		const resultPartOne = solvePartOne('input_example.txt')
+
+		expect(resultPartOne).toBe(4512)
+	})
 
 
+	it('part2: should solve part two for test example', () => {
+		const resultPartOne = solvePartTwo('input_example.txt')
+
+		expect(resultPartOne).toBe(1924)
+	})
 })
