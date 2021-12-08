3493fbf1e44aaa4f72a1a526765dd1859ee3abe8
Gabriel Checchia Vitali <vitali.gabriel@gmail.com>
2021-12-08 16:42:10 +0100

    AoC-5: [@diesdasjenes, @gabcvit] Implemented part 1 partially (tests from example are working, final input isn't)
    some context/description

---
+++ 2021/d_05/index.js
@@ -0,0 +1,115 @@
+var fs = require('fs');
+
+const generateInputFromFile = (fileName) => {
+	try {
+		const data = fs.readFileSync(fileName, 'utf8');
+		return data.toString().split('\n').map(singleLine => {
+			let matches = singleLine.match(/([0-9]+),([0-9]+) -> ([0-9]+),([0-9]+)/)
+			return {
+				x1: parseInt(matches[1]),
+				y1: parseInt(matches[2]),
+				x2: parseInt(matches[3]),
+				y2: parseInt(matches[4])
+			}
+		})
+	} catch(e) {
+		console.log('Error:', e.stack);
+	}
+}
+
+const isDiagonal = (vector) => {
+	return vector.x1 !== vector.x2 && vector.y1 !== vector.y2
+}
+
+const getPointsToCover = (number1, number2) => {
+	const range = (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i);
+
+	let sortedArray = [number1, number2].sort()
+	return range(sortedArray[0], sortedArray[1])
+}
+
+const getPointsCoveredByVector = (vector) => {
+	if (isDiagonal(vector)) {
+		return null
+	}
+
+	let pointsToCover = []
+	
+	if (vector.x1 !== vector.x2) {
+		pointsToCover = getPointsToCover(vector.x1, vector.x2).map(singleXValue => {
+			return `${singleXValue}-${vector.y1}`
+		})
+	} else {
+		pointsToCover = getPointsToCover(vector.y1, vector.y2).map(singleYValue => {
+			return `${vector.x1}-${singleYValue}`
+		})
+	}
+
+	return pointsToCover
+}
+
+const generateHashMap = (pointCoordinates, hashMap) => {
+	pointCoordinates.forEach(singlePointCoordinate => {
+		let currentValueOfPoint = hashMap.get(singlePointCoordinate)
+
+		if (currentValueOfPoint) {
+			currentValueOfPoint++
+		} else {
+			currentValueOfPoint = 1
+		}
+
+		hashMap.set(singlePointCoordinate, currentValueOfPoint)
+	})
+
+
+	return hashMap
+}
+
+const getAllCrossroadsFromHashMap = (hashMap) => {
+	let countOfCrossroads = 0
+
+	for (singlePoint of hashMap) {
+		if (singlePoint[1] >= 2) {
+			countOfCrossroads++
+		}
+	}
+
+	return countOfCrossroads
+}
+
+const solvePartOne = (filename) => {
+	const inputArray = generateInputFromFile(filename)
+
+	let allPointsToIncrement = []
+	for (let singleVector of inputArray) {
+		let points = getPointsCoveredByVector(singleVector)
+		if (points !== null) {
+			allPointsToIncrement = allPointsToIncrement.concat(points)
+		}
+	}
+	
+	let hashMap = new Map()
+	hashMap = generateHashMap(allPointsToIncrement, hashMap)
+	return getAllCrossroadsFromHashMap(hashMap)
+}
+
+// RUN THE CHALLENGE
+
+// pt1
+// 4687 is too low
+// 4797 is wrong
+console.log('part 1: ', solvePartOne('input_final.txt'))
+
+//pt 2
+// console.log('part 2: ', solvePartTwo('input_final.txt'))
+
+
+
+
+module.exports = {
+	generateInputFromFile,
+	getPointsCoveredByVector,
+	generateHashMap,
+	getAllCrossroadsFromHashMap,
+	solvePartOne
+}
---
+++ 2021/d_05/index.test.js
@@ -0,0 +1,62 @@
+const { it, expect } = require("@jest/globals")
+const { generateInputFromFile, getPointsCoveredByVector, generateHashMap, getAllCrossroadsFromHashMap, solvePartOne } = require("./index.js")
+
+
+describe('Testing index functionaities', () => {
+	it('part1: should split the input file into a valid array', () => {
+		const inputArray = generateInputFromFile('input_example.txt')
+		const expectedFirstElement = { x1: 0, y1: 9, x2: 5, y2: 9 }
+		console.log(inputArray)
+		expect(inputArray.length).toBe(10)
+		expect(inputArray[0]).toEqual(expectedFirstElement)
+	})
+
+	it('part1: should return a valid array of points to be incremented for a vector', () => {
+		const vector = { x1: 0, y1: 9, x2: 5, y2: 9 }
+		const result = getPointsCoveredByVector(vector)
+		const expectedResult = ['0-9', '1-9', '2-9', '3-9', '4-9', '5-9']
+
+		console.log(result)
+		expect(result).toEqual(expectedResult)
+	})
+
+	it('part1: should add all point coordinates into a hash map', () => {
+		const points = ['1-1','1-2','1-3','0-3','0-2','0-1']
+		const hashMap = new Map()
+		generateHashMap(points, hashMap)
+
+		let expectedResult = new Map()
+		expectedResult.set('1-1', 1);
+		expectedResult.set('1-2', 1);
+		expectedResult.set('1-3', 1);
+		expectedResult.set('0-3', 1);
+		expectedResult.set('0-2', 1);
+		expectedResult.set('0-1', 1);
+
+		expect(hashMap).toEqual(expectedResult)
+	})
+
+	it('part1: count all crossroads', () => {
+		let hashMap = new Map()
+		hashMap.set('1-1', 1);
+		hashMap.set('1-2', 2);
+		hashMap.set('1-3', 2);
+		hashMap.set('0-3', 1);
+		hashMap.set('0-2', 1);
+		hashMap.set('0-1', 3);
+
+		const result = getAllCrossroadsFromHashMap(hashMap)
+
+		expect(result).toBe(3)
+	})
+
+	it('part1: should pass example', () => {
+		const result = solvePartOne('input_final.txt')
+		const expectedResult = 5
+
+		expect(result).toBe(expectedResult)
+	})
+
+
+
+})
